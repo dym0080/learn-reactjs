4839ebccd8c1893a53f1c56e338f85bfaf20a4a4
dym0080 <308960474@qq.com>
2019-09-11 09:23:59 +0800

    Add antd-app when learning Ant Design

---
+++ antd-app/src/app.js
@@ -0,0 +1,20 @@
+// export const dva = {
+//   config: {
+//     onError(err) {
+//       err.preventDefault();
+//       console.error(err.message);
+//     },
+//   },
+// };
+
+export const dva = {
+  config: {
+    onError(err) {
+      err.preventDefault();
+      console.error(err.message);
+    },
+    initialState: {
+      products: [{ name: 'dva', id: 1 }, { name: 'antd', id: 2 }],
+    },
+  },
+};
